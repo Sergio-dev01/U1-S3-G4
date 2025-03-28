1b6f70745244c73c5290decf8ac84968652e478f
Sergio Maselli <sergio.develoop@gmail.com>
2025-03-28 09:01:02 +0100

    add mod

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-function createTable() {
-  const table = document.getElementById("board");
+const appNums = [];
+let availableNumbers = Array.from({ length: 90 }, (_, i) => i + 1);
+
+function tableMaker() {
+  const tableBlock = document.getElementById("table");
 
   for (let i = 1; i <= 90; i++) {
-    const td = document.createElement("div");
-    td.classList.add("cell");
-    td.innerText = i;
-    td.dataset.numero = i;
+    const tD = document.createElement("div");
+    tD.classList.add("cells");
+    tD.textContent = i;
 
-    table.appendChild(td);
+    tableBlock.appendChild(tD);
   }
 }
 
-function getNumber() {
-  const randomNum = Math.floor(Math.random() * 90) + 1;
-  const cells = document.querySelectorAll(".cell");
-  return randomNum;
-}
-const bottone = document.getElementById("extract-button");
-bottone.addEventListener("click", getNumber);
+function getNum() {
+  if (availableNumbers.length === 0) {
+    alert("Hai estratto tutti i numeri!");
+    return;
+  }
 
-createTable();
+  const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+  const numRandom = availableNumbers.splice(randomIndex, 1)[0];
+
+  appNums.push(numRandom);
+  getCell(numRandom);
+}
+
+function getCell(num) {
+  const tDivs = document.querySelectorAll(".cells");
+
+  for (let i = 0; i < tDivs.length; i++) {
+    if (parseInt(tDivs[i].textContent) === num) {
+      tDivs[i].classList.add("inEvidenza");
+    }
+  }
+}
+
+const pulsante = document.getElementById("extraction-button");
+pulsante.addEventListener("click", getNum);
+
+tableMaker();
